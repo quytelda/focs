47d802e03d41db05d24ac7c0b772b853cab9b175
Quytelda Kahja <quytelda@tamalin.org>
2018-04-15 18:06:08 -0700

    test: list: linked_list: Add unit tests for 'linklist_insert()'.

--- test/list/linked_list.c
+++ test/list/linked_list.c
@@ -194,6 +194,60 @@ START_TEST(test_linklist_pop_tail_multiple)
 }
 END_TEST
 
+START_TEST(test_linklist_insert_single)
+{
+	uint8_t in = 1;
+	bool success;
+	struct linked_list * list;
+
+	linklist_alloc(&list, sizeof(uint8_t));
+
+	success = linklist_insert(list, &in, 0);
+
+	ck_assert(success);
+	ck_assert(list->head);
+	ck_assert(list->tail);
+
+	linklist_free(&list);
+}
+END_TEST
+
+START_TEST(test_linklist_insert_multiple)
+{
+	bool r1;
+	bool r2;
+	bool r3;
+	bool r4;
+	uint8_t in1 = 1;
+	uint8_t in2 = 2;
+	uint8_t in3 = 3;
+	uint8_t in4 = 4;
+	struct linked_list * list;
+
+	linklist_alloc(&list, sizeof(uint8_t));
+
+	/* Insert the elements out of order. */
+	r1 = linklist_insert(list, &in1, 0);
+	r2 = linklist_insert(list, &in3, 1);
+	r3 = linklist_insert(list, &in2, 1);
+	r4 = linklist_insert(list, &in4, 0);
+
+	ck_assert(r1);
+	ck_assert(r2);
+	ck_assert(r3);
+	ck_assert(r4);
+
+	ck_assert(list->head);
+	ck_assert(list->tail);
+
+	ck_assert_mem_eq(linklist_pop_head(list), &in4, sizeof(uint8_t));
+	ck_assert_mem_eq(linklist_pop_head(list), &in1, sizeof(uint8_t));
+	ck_assert_mem_eq(linklist_pop_head(list), &in2, sizeof(uint8_t));
+	ck_assert_mem_eq(linklist_pop_head(list), &in3, sizeof(uint8_t));
+
+	linklist_free(&list);
+}
+END_TEST
 
 Suite * linklist_suite(void)
 {
@@ -204,6 +258,7 @@ Suite * linklist_suite(void)
 	TCase * case_linklist_push_tail;
 	TCase * case_linklist_pop_head;
 	TCase * case_linklist_pop_tail;
+	TCase * case_linklist_insert;
 
 	suite = suite_create("Linked List");
 
@@ -213,6 +268,7 @@ Suite * linklist_suite(void)
 	case_linklist_push_tail = tcase_create("linklist_push_tail");
 	case_linklist_pop_head = tcase_create("linklist_pop_head");
 	case_linklist_pop_tail = tcase_create("linklist_pop_tail");
+	case_linklist_insert = tcase_create("linklist_insert");
 
 	tcase_add_test(case_linklist_alloc, test_linklist_alloc);
 	tcase_add_test(case_linklist_null, test_linklist_null_true);
@@ -224,14 +280,16 @@ Suite * linklist_suite(void)
 	tcase_add_test(case_linklist_pop_head, test_linklist_pop_head_single);
 	tcase_add_test(case_linklist_pop_tail, test_linklist_pop_tail_multiple);
 	tcase_add_test(case_linklist_pop_tail, test_linklist_pop_tail_single);
+	tcase_add_test(case_linklist_insert, test_linklist_insert_multiple);
+	tcase_add_test(case_linklist_insert, test_linklist_insert_single);
 
 	suite_add_tcase(suite, case_linklist_alloc);
 	suite_add_tcase(suite, case_linklist_null);
 	suite_add_tcase(suite, case_linklist_push_head);
 	suite_add_tcase(suite, case_linklist_push_tail);
-	suite_add_tcase(suite, case_linklist_pop_head);	
+	suite_add_tcase(suite, case_linklist_pop_head);
 	suite_add_tcase(suite, case_linklist_pop_tail);
-	
+	suite_add_tcase(suite, case_linklist_insert);
 
 	return suite;
 }
