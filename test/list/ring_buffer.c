c584606636d266200e6a9eb88d139a0753bec01d
Quytelda Kahja <quytelda@tamalin.org>
2018-04-28 15:36:27 -0700

    test: ring_buffer: Add unit tests for 'rb_push_head/tail()'.
    These unit tests are basic and will need to be expanded later when
    more ring_buffer API functions are available to check the data that
    was inserted into the list.

--- test/list/ring_buffer.c
+++ test/list/ring_buffer.c
@@ -43,18 +43,104 @@ START_TEST(test_rb_alloc)
 }
 END_TEST
 
+START_TEST(test_rb_push_head_single)
+{
+	bool success;
+	uint8_t in = 1;
+	struct ring_buffer * buf = NULL;
+
+	rb_alloc(&buf, &props);
+
+	success = rb_push_head(buf, &in);
+
+	ck_assert(success);
+	ck_assert_int_eq(buf->length, 1);
+
+	rb_free(&buf);
+}
+END_TEST
+
+START_TEST(test_rb_push_head_multiple)
+{
+	bool success[3];
+	uint8_t in[] = {1, 2, 3};
+	struct ring_buffer * buf = NULL;
+
+	rb_alloc(&buf, &props);
+
+	success[0] = rb_push_head(buf, &in[0]);
+	success[1] = rb_push_head(buf, &in[1]);
+	success[2] = rb_push_head(buf, &in[2]);
+
+	ck_assert(success[0]);
+	ck_assert(success[1]);
+	ck_assert(success[2]);
+	ck_assert_int_eq(buf->length, 3);
+
+	rb_free(&buf);
+}
+END_TEST
+
+START_TEST(test_rb_push_tail_single)
+{
+	bool success;
+	uint8_t in = 1;
+	struct ring_buffer * buf = NULL;
+
+	rb_alloc(&buf, &props);
+
+	success = rb_push_tail(buf, &in);
+
+	ck_assert(success);
+	ck_assert_int_eq(buf->length, 1);
+
+	rb_free(&buf);
+}
+END_TEST
+
+START_TEST(test_rb_push_tail_multiple)
+{
+	bool success[3];
+	uint8_t in[] = {1, 2, 3};
+	struct ring_buffer * buf = NULL;
+
+	rb_alloc(&buf, &props);
+
+	success[0] = rb_push_tail(buf, &in[0]);
+	success[1] = rb_push_tail(buf, &in[1]);
+	success[2] = rb_push_tail(buf, &in[2]);
+
+	ck_assert(success[0]);
+	ck_assert(success[1]);
+	ck_assert(success[2]);
+	ck_assert_int_eq(buf->length, 3);
+
+	rb_free(&buf);
+}
+END_TEST
+
 Suite * rb_suite(void)
 {
 	Suite * suite;
 	TCase * case_rb_alloc;
+	TCase * case_rb_push_head;
+	TCase * case_rb_push_tail;
 
 	suite = suite_create("Ring Buffer");
 
 	case_rb_alloc = tcase_create("rb_alloc");
+	case_rb_push_head = tcase_create("rb_push_head");
+	case_rb_push_head = tcase_create("rb_push_tail");
 
 	tcase_add_test(case_rb_alloc, test_rb_alloc);
+	tcase_add_test(case_rb_push_head, test_rb_push_head_single);
+	tcase_add_test(case_rb_push_head, test_rb_push_head_multiple);
+	tcase_add_test(case_rb_push_tail, test_rb_push_tail_single);
+	tcase_add_test(case_rb_push_tail, test_rb_push_tail_multiple);
 
 	suite_add_tcase(suite, case_rb_alloc);
+	suite_add_tcase(suite, case_rb_push_head);
+	suite_add_tcase(suite, case_rb_push_tail);
 
 	return suite;
 }
