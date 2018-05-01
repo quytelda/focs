8616eb65be410d150f2fe4a78696c1a1843d1a14
Quytelda Kahja <quytelda@tamalin.org>
2018-04-30 19:42:15 -0700

    focs: Add modulo operation that always returns a positive integer.
    The mathematically defined range for the modulo operation is
    0 ≤ (a mod n) < n.  ISO C99 defines the '%' operator to return the
    REMAINDER of a it's quotient, which might be negative depending on
    the sign of the types; this behavior is unwanted in situations where
    the program must work with modular (looping) array indices.  Add a
    macro `mod` that calculates the modulo operation correctly.

--- include/focs.h
+++ include/focs.h
@@ -26,6 +26,23 @@
 #ifndef __FOCS_H
 #define __FOCS_H
 
+/**
+ * mod() - Modulo operation
+ * @a The divisor
+ * @n The dividend
+ *
+ * Returns the remainder of the quotient `(@a / @n)`.  This operation is
+ * guaranteed to always return a positive integer.  Both @a and @n should be
+ * signed types if either @a or @n may be negative.
+ *
+ * This modulo operation is distinct from the ISO C99 remainder operation
+ * represented by the operator '%', which will produce negative results
+ * if the divisor is negative and always takes the sign of the dividend.
+ *
+ * See: https://en.wikipedia.org/wiki/Modulo_operation
+ */
+#define mod(a, n) ((a % n + n) % n)
+
 typedef void * (* map_fn_t)(void * data);
 typedef void * (* foldr_fn_t)(const void * c, void * acc);
 typedef void * (* foldl_fn_t)(void * acc, const void * c);
