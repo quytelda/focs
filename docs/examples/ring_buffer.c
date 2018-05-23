0e20b74a652f1fce088c419609237fb3bba2f019
Quytelda Kahja <quytelda@tamalin.org>
2018-05-23 15:13:32 -0700

    docs: Add ring buffer example.

---
+++ docs/examples/ring_buffer.c
@@ -0,0 +1,77 @@
+/* ring_buffer.c - Reverse and take the absolute value of a list of numbers
+ *                 using a doubly linked list as a stack.
+ * Copyright (C) 2018 Quytelda Kahja
+ *
+ * This file is part of focs.
+ *
+ * focs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * focs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with focs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <list/ring_buffer.h>
+#include <focs/generics/hof.h>
+
+static const struct ds_properties props = {
+	.data_size = sizeof(int),
+	.entries   = 10,
+	.overwrite = true,
+};
+
+static int my_abs(int n)
+{
+	if(n < 0)
+		n = (-n);
+
+	return n;
+}
+WRAP_MAPPABLE(my_abs, int, my_abs_mappable);
+
+int main(int argc, char * argv[])
+{
+	int *  inputs;
+	int ** outputs;
+
+	ring_buffer my_list;
+
+	inputs  = calloc(argc, sizeof(int));
+	outputs = calloc(argc, sizeof(int *));
+	if(!inputs || !outputs)
+		return -1; // out of memory
+
+	my_list = rb_create(&props);
+
+	/* Push the numbers onto the stack. */
+	for(int i = 1; i < argc; i++) {
+		inputs[i] = atoi(argv[i]);
+		rb_push_head(my_list, &inputs[i]);
+	}
+
+	/* Transform the list by mapping my_abs over the list. */
+	map(my_list, my_abs_mappable);
+
+	/* Output each number popped from the stack. */
+	size_t length = rb_size(my_list);
+	for(int i = 0; i < length; i++) {
+		outputs[i] = rb_pop_head(my_list);
+		printf("%d ", *outputs[i]);
+	}
+
+	putchar('\n');
+
+	free(inputs);
+	free(outputs);
+	rb_destroy(&my_list);
+	return 0;
+}
